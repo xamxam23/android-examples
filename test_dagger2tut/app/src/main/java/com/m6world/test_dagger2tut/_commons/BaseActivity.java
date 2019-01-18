1ea14afb4e6adfccc1a3429ca6ecfa4b36c3f203
Maxime Mbabele <mmbabele@gmail.com>
2019-01-18 08:19:34 +0200

    Dagger 2

--- test_dagger2tut/app/src/main/java/com/m6world/test_dagger2tut/_commons/BaseActivity.java
+++ test_dagger2tut/app/src/main/java/com/m6world/test_dagger2tut/_commons/BaseActivity.java
@@ -2,6 +2,4 @@ package com.m6world.test_dagger2tut._commons;
 
 import dagger.android.DaggerActivity;
 
-public class BaseActivity extends DaggerActivity {
-
-}
+public class BaseActivity extends DaggerActivity {}
